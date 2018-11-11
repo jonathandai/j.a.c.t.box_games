5e43a3efc201622b1fee967a5e0b9ae09a52e817
Conor McGeehan <conorm15@gmail.com>
2018-11-10 22:02:20 -0600

    Update jactbox-games/src/PhoneCanvas.js

--- jactbox-games/src/PhoneCanvas.js
+++ jactbox-games/src/PhoneCanvas.js
@@ -5,18 +5,6 @@ import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 class PhoneCanvas extends Component {
 
-
-  var canvasDiv = document.getElementById('canvasDiv');
-  canvas = document.createElement('canvas');
-  canvas.setAttribute('width', canvasWidth);
-  canvas.setAttribute('height', canvasHeight);
-  canvas.setAttribute('id', 'canvas');
-  canvasDiv.appendChild(canvas);
-  if(typeof G_vmlCanvasManager != 'undefined') {
-  	canvas = G_vmlCanvasManager.initElement(canvas);
-  }
-  context = canvas.getContext("2d");
-
   render() {
     return(
       <div className="phone">
