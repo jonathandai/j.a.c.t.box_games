ef8dc5ecf72ccfe336e6de4895b6aca0fd60f270
Conor McGeehan <conorm15@gmail.com>
2018-11-10 22:02:05 -0600

    Update jactbox-games/src/PhoneCanvas.js

--- jactbox-games/src/PhoneCanvas.js
+++ jactbox-games/src/PhoneCanvas.js
@@ -4,65 +4,26 @@ import './phone.css';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 class PhoneCanvas extends Component {
+
+
+  var canvasDiv = document.getElementById('canvasDiv');
+  canvas = document.createElement('canvas');
+  canvas.setAttribute('width', canvasWidth);
+  canvas.setAttribute('height', canvasHeight);
+  canvas.setAttribute('id', 'canvas');
+  canvasDiv.appendChild(canvas);
+  if(typeof G_vmlCanvasManager != 'undefined') {
+  	canvas = G_vmlCanvasManager.initElement(canvas);
+  }
+  context = canvas.getContext("2d");
+
   render() {
     return(
       <div className="phone">
-        <div className="canvas"></div>
+        <div className="canvasDiv"></div>
       </div>
   )
   }
 }
 
 export default PhoneCanvas;
-
-var drawingApp = (function () {
-
-	"use strict";
-
-	var canvas,
-		context,
-		canvasWidth = 667,
-		canvasHeight = 375,
-		colorPurple = "#cb3594",
-		colorGreen = "#659b41",
-		colorYellow = "#ffcf33",
-		colorBrown = "#986928",
-		outlineImage = new Image(),
-		crayonImage = new Image(),
-		markerImage = new Image(),
-		eraserImage = new Image(),
-		crayonBackgroundImage = new Image(),
-		markerBackgroundImage = new Image(),
-		eraserBackgroundImage = new Image(),
-		crayonTextureImage = new Image(),
-		clickX = [],
-		clickY = [],
-		clickColor = [],
-		clickTool = [],
-		clickSize = [],
-		clickDrag = [],
-		paint = false,
-		curColor = colorPurple,
-		curTool = "crayon",
-		curSize = "normal",
-		mediumStartX = 18,
-		mediumStartY = 19,
-		mediumImageWidth = 93,
-		mediumImageHeight = 46,
-		drawingAreaX = 111,
-		drawingAreaY = 11,
-		drawingAreaWidth = 267,
-		drawingAreaHeight = 200,
-		toolHotspotStartY = 23,
-		toolHotspotHeight = 38,
-		sizeHotspotStartY = 157,
-		sizeHotspotHeight = 36,
-		totalLoadResources = 8,
-		curLoadResNum = 0,
-		sizeHotspotWidthObject = {
-			huge: 39,
-			large: 25,
-			normal: 18,
-			small: 16
-		}
-})
