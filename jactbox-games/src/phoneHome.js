117f5f3ecf1d2bc4c0c92747e55d214c1a6fe650
Conor McGeehan <conorm15@gmail.com>
2018-11-10 18:05:37 -0600

    ya

--- jactbox-games/src/phoneHome.js
+++ jactbox-games/src/phoneHome.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import firebase from './firebase.js';
 import './phone.css';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 class PhoneHome extends Component {
   render() {
