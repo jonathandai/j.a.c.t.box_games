e4f1b787c9771b2ffb0b31dce6fbf3ada241c962
Abby Pratt <abbypratt@Abbys-MacBook-Pro.local>
2018-11-10 21:43:15 -0600

    center align
    fixed alignment

--- jactbox-games/src/Question.js
+++ jactbox-games/src/Question.js
@@ -5,21 +5,30 @@ class Question extends Component {
     return(
     	<div>
             <div className='topBar'>
-           	  <header>
-              <h1>YOUR CHALLENGE IS TO...</h1>
-              </header>
+              YOUR CHALLENGE IS TO...
               </div>
-              <div className='problem'>
+
+              
+ 			 <div class="column">
+ 			 <div className='problem'>
               find a way for <br/><br/><br/>
               to <br/><br/><br/>
               that is
-            </div>
-            <div>
-            
-            </div>
+              </div>
+             </div>
+			 <div class="column">
+			<div className='topics'>
+            some person <br/><br/>
+            some problem<br/><br/>
+            some constraint<br/><br/>
+          
+			</div>
+			</div>
+                         
          </div>
   )
   }
 }
 
 export default Question;
+
